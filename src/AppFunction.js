c9a9be9a83d8fa12d3b733f87598942cf69ad1de
Waga Odongo <wagaodongo@gmail.com>
2019-03-18 13:26:34 +0300

    UseEffects
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,13 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AppFunction = () => {
 	const [count, setCount] = useState(0);
+	const [isOn, setIsOn] = useState(false);
+
+	useEffect(() => {
+		document.title = `You have clicked ${count} times`;
+	});
 
 	const incrementCount = () => {
-		setCount(count + 1);
+		setCount((prevCount) => prevCount + 1);
 	};
 
-	return <button onClick={incrementCount}>I was clicked {count} times</button>;
+	const toggleLight = () => {
+		setIsOn((prevIson) => !prevIson);
+	};
+
+	return (
+		<>
+			<h2>Counter </h2>
+			<button onClick={incrementCount}>I was clicked {count} times</button>
+
+			<h2> Toggle Light </h2>
+			<div
+				style={{
+					height: "50px",
+					width: "50px",
+					background: isOn ? "yellow" : "grey"
+				}}
+				onClick={toggleLight}
+			/>
+		</>
+	);
 };
 
 export default AppFunction;
