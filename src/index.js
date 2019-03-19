ffa58bb79623f73d500aa11a8d03fbd35ec94602
Waga Odongo <wagaodongo@gmail.com>
2019-03-19 09:20:26 +0300

    register page
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 //import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Login from "./Login";
+import Register from "./Register";
+//import Login from "./Login";
 //import AppFunction from "./AppFunction";
 
-ReactDOM.render(<Login />, document.getElementById("root"));
+ReactDOM.render(<Register />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
